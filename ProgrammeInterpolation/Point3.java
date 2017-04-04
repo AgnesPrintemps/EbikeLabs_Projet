a315ababae69a72e1caed9fd9ed950013f99666f
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-04 15:02:39 +0200

    Create Point3.java

---
+++ ProgrammeInterpolation/Point3.java
@@ -0,0 +1,18 @@
+
+class Point3 extends Point{
+		
+	// constructeur
+	public Point3(double x, double y, double yprime){
+		super(x,y);
+		this.yprime=yprime;
+	}
+	
+	/** retourne la pente du point
+	 * @return la pente du point
+	 */
+	public double getyprime(){
+		return yprime;
+	}
+	
+	private double yprime; // pente
+}
