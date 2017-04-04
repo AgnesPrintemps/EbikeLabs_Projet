1eec5826620ec9b052b9fec72a99c823d72b9131
AgnesPrintemps <agnes-printemps@hotmail.fr>
2017-04-04 10:04:51 +0200

    Update GrayImage.java
    Méthode calcul d'aire qui renvoie un double

--- ExtraireImage/src/GrayImage.java
+++ ExtraireImage/src/GrayImage.java
@@ -14,6 +14,7 @@ public class GrayImage extends JPanel {
 	private static final int TYPE_INT_ARGB=2;
 	private BufferedImage A;
 	private final int BLANC=230;
+	private int nbpixelsnonblancs;
 	
 	  protected void paintComponent(Graphics g) {
 		    int x = (getWidth() - A.getWidth()) / 2;
@@ -45,31 +46,32 @@ public class GrayImage extends JPanel {
 		//compteur de pixel non "blanc"
 		int compteur=0;
 		//parcourir tous les pixels de la photo
-    	for(int i=0;i<A.getWidth();i++){
-    		for(int j=0;j<A.getHeight();j++){
+    		for(int i=0;i<A.getWidth();i++){
+    			for(int j=0;j<A.getHeight();j++){
+    				rgb=A.getRGB(i,j);
     			
-    			rgb=A.getRGB(i,j);
-    			
-    			//pour extraire les couleurs du pixel
-    			alpha = (rgb >> 24) & 0xFF;
-    			rouge =   (rgb >> 16) & 0xFF;
-    			vert = (rgb >>  8) & 0xFF;
-    			bleu =  (rgb >> 0) & 0xFF;
-    			if(rouge<BLANC || vert<BLANC || bleu<BLANC){
-    				compteur++;
+    				//pour extraire les couleurs du pixel
+    				alpha = (rgb >> 24) & 0xFF;
+    				rouge =   (rgb >> 16) & 0xFF;
+    				vert = (rgb >>  8) & 0xFF;
+    				bleu =  (rgb >> 0) & 0xFF;
+    				if(rouge<BLANC || vert<BLANC || bleu<BLANC){
+    					nbpixelsnonblancs++;
+    				}else{
+    					A.setRGB(i, j, 0);
+    				}
+    				//System.out.println(A.getRGB(i, j));
     			}
-    			else{
-    				A.setRGB(i, j, 0);
-    			}
-    			//System.out.println(A.getRGB(i, j));
-    			
     		}
-    	}
-    	
-    	//pourcentage de pixel non blanc
-    	double pourcentage=((double)compteur/(double)((A.getWidth()*A.getHeight())))*100;
-    	System.out.println(pourcentage);
-    	
+		
+    		//pourcentage de pixel non blanc
+    		//double pourcentage=((double)compteur/(double)((A.getWidth()*A.getHeight())))*100;
+    		//System.out.println(pourcentage);
+	}
+	
+	public double Aire (){
+		return ((double)compteur/(double)((A.getWidth()*A.getHeight())))*100;
+	}
     	 // cree une fenetre contenant l'image
     	 JFrame f = new JFrame();
     	 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
