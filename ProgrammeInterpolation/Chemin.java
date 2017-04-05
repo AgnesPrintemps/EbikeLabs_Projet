365f763b5b5ed3df7330214527e74c4c1e7d1759
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-05 13:00:31 +0200

    Add files via upload

--- ProgrammeInterpolation/Chemin.java
+++ ProgrammeInterpolation/Chemin.java
@@ -14,22 +14,30 @@ class Chemin{
 		File f = new File(nom); // on prend le fichier correspondant à la chaine de caractères
 		LinkedList<Point> l = new LinkedList<Point>(); // les distances à l'origine
 		t = new LinkedList<Point>(); // les paramètres a et b
-		//try{
-			Scanner sc=new Scanner(System.in).useLocale(Locale.US);
-			while(sc.hasNextDouble()){ // tant qu'il reste encore des réels dans le fichier
-				double dis=sc.nextDouble(); // on prend la distance à l'origine
-				double al=sc.nextDouble(); // on prend son altitude
-				l.add(new Point(dis,al)); // et on les stocke dans un point qu'on rajoute au chemin
-				if(sc.hasNextDouble()){
-					double a=sc.nextDouble(); // on prend le paramètre a
-					double b=sc.nextDouble(); // on prend le paramètre b
-					t.add(new Point(a-b,b)); // et on les stocke dans un point qu'on rajoute à la liste des paramètres
+		try{
+			double dis=0.0;
+			double alt=0.0;
+			double a=1.0;
+			double b=0.0;
+			Scanner sc=new Scanner(f).useLocale(Locale.US);;
+			while(sc.hasNextLine()){ // tant qu'il reste encore des lignes dans le fichier
+				String temp = sc.nextLine();
+				String[] nums= temp.split(" ");
+				dis=Double.parseDouble(nums[0]); // On récupère la distance à l'origine
+				alt=Double.parseDouble(nums[1]); // On récupère l'altitude
+				l.add(new Point(dis,alt)); // et on les stocke dans un point qu'on rajoute au chemin
+				if(nums.length==4){ // si il y a 4 et non pas 2 paramètres sur la ligne
+					a=Double.parseDouble(nums[2]); // on prend le paramètre a
+					b=Double.parseDouble(nums[3]); // on prend le paramètre b
 				}
+				// sinon, on garde ceux d'avant
+				t.add(new Point(a,b)); // et on les stocke dans un point qu'on rajoute à la liste des paramètres
 			}
-		/*}
+			sc.close();
+		}
 		catch(FileNotFoundException e){
 			e.printStackTrace();
-		}*/
+		}
 		p = new LinkedList<Point3>();
 		p.add(new Point3(l.getFirst().getx(),l.getFirst().gety(),0)); // on met la première pente à 0
 		for(int i=1; i<l.size()-1;i++){ // pour chaque point, on trouve sa pente en fonction des points d'avant et d'après
