9e0486c8ffd0071ee3b404df0a52aa3c2e3db27a
Chocolulu <lucie.basquin@gmail.com>
2017-04-05 14:23:58 +0200

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -19,137 +20,98 @@ public class ApplicationEbike extends JFrame implements ActionListener{
 	private JTextField T2;	
 	private JTextField T3;
 	private JTextField T4;
+	private JTextField T5;
+	private JTextField T6;
+	private JTextField T7;
+	private JTextField T8;
 	private JLabel L1;
 	private JLabel L2;
 	private JLabel L3;
 	private JLabel L4;
+	private JLabel L5;
+	private JLabel L6;
+	private JLabel L7;
+	private JLabel L8;
 	
 	private final static String newline = "\n";
 	
 	public ApplicationEbike() {
-		this.setTitle("GridBagLayout");
-	    this.setSize(1700, 1000);
-	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    this.setLocationRelativeTo(null);
-		//JFrame Fen = new JFrame();
-//	    Fen.setLayout(new GridLayout(10, 4));
-//		T1 = new JTextField(30);
-//		T2 = new JTextField(30);
-//		T3 = new JTextField(10);
-//		T4 = new JTextField(10);
-//		L1 = new JLabel("Quel est votre point de départ? ");
-//		L2 = new JLabel("Quel est votre point d'arrivée? ");
-//		L3 = new JLabel("Quelle est votre taille en cm? ");
-//		L4 = new JLabel("Quel est votre poids en kg? ");
-//	    Fen.getContentPane().add(T1);
-//	    Fen.getContentPane().add(T2);
-//	    Fen.getContentPane().add(T3);
-//	    Fen.getContentPane().add(T4);
-//	    Fen.getContentPane().add(L1);
-//	    Fen.getContentPane().add(L2);
-//	    Fen.getContentPane().add(L3);
-//	    Fen.getContentPane().add(L4);
-		//fen.getContentPane().add(new JButton("3"));
-		//this.getContentPane().add(new JButton("4"));
-		//this.getContentPane().add(new JButton("5"));
-	  //On crée nos différents conteneurs de couleur différente
-	    JPanel cell1 = new JPanel();
-	    cell1.setBackground(Color.YELLOW);
-	    cell1.setPreferredSize(new Dimension(150, 40));		
-	    JPanel cell2 = new JPanel();
-	    cell2.setBackground(Color.red);
-	    cell2.setPreferredSize(new Dimension(60, 40));
-	    JPanel cell3 = new JPanel();
-	    cell3.setBackground(Color.green);
-	    cell3.setPreferredSize(new Dimension(60, 40));
-	    JPanel cell4 = new JPanel();
-	    cell4.setBackground(Color.black);
-	    cell4.setPreferredSize(new Dimension(60, 40));
-	    JPanel cell5 = new JPanel();
-	    cell5.setBackground(Color.cyan);
-	    cell5.setPreferredSize(new Dimension(60, 40));
-	    JPanel cell6 = new JPanel();
-	    cell6.setBackground(Color.BLUE);
-	    cell6.setPreferredSize(new Dimension(60, 40));
-	    JPanel cell7 = new JPanel();
-	    cell7.setBackground(Color.orange);
-	    cell7.setPreferredSize(new Dimension(60, 40));
-	    JPanel cell8 = new JPanel();
-	    cell8.setBackground(Color.DARK_GRAY);
-	    cell8.setPreferredSize(new Dimension(60, 40));
-	    //Le conteneur principal
-	    JPanel content = new JPanel();
-	    content.setPreferredSize(new Dimension(1700, 1000));
-	    content.setBackground(Color.WHITE);
-	    //On définit le layout manager
-	    content.setLayout(new GridBagLayout());
 		
-	    //L'objet servant à positionner les composants
-	    GridBagConstraints gbc = new GridBagConstraints();
-			
-	    //On positionne la case de départ du composant
-	    gbc.gridx = -00;
-	    gbc.gridy = -10;
-	    //La taille en hauteur et en largeur
-	    gbc.gridheight = 1;
-	    gbc.gridwidth = 1;
-	    content.add(cell1, gbc);
-	    //---------------------------------------------
-	    gbc.gridx = 1;
-	    content.add(cell2, gbc);
-	    //---------------------------------------------
-	    gbc.gridx = 2;		
-	    content.add(cell3, gbc);		
-	    //---------------------------------------------
-	    //Cette instruction informe le layout que c'est une fin de ligne
-	    gbc.gridwidth = GridBagConstraints.REMAINDER;
-	    gbc.gridx = 3;	
-	    content.add(cell4, gbc);
-	    //---------------------------------------------
-	    gbc.gridx = 0;
-	    gbc.gridy = 1;
-	    gbc.gridwidth = 1;
-	    gbc.gridheight = 2;
-	    //Celle-ci indique que la cellule se réplique de façon verticale
-	    gbc.fill = GridBagConstraints.VERTICAL;
-	    content.add(cell5, gbc);
-	    //---------------------------------------------
-	    gbc.gridx = 1;
-	    gbc.gridheight = 1;
-	    //Celle-ci indique que la cellule se réplique de façon horizontale
-	    gbc.fill = GridBagConstraints.HORIZONTAL;
-	    gbc.gridwidth = GridBagConstraints.REMAINDER;
-	    content.add(cell6, gbc);
-	    //---------------------------------------------
-	    gbc.gridx = 1;
-	    gbc.gridy = 2;
-	    gbc.gridwidth = 2;
-	    content.add(cell7, gbc);
-	    //---------------------------------------------
-	    gbc.gridx = 3;
-	    gbc.gridwidth = GridBagConstraints.REMAINDER;
-	    content.add(cell8, gbc);
-	    //---------------------------------------------
-	    //On ajoute le conteneur
-	    this.setContentPane(content);
 
-	    this.setVisible(true);
-	//	Fen.add(T1); Fen.add(T2);
-//		Fen.setTitle("Project Ebike");
-//		Fen.setSize(1700, 1000);
-//		Fen.setVisible(true);
-	//	T1.addActionListener(this);
-	//	T2.addActionListener(this);
-//		T3.addActionListener(this);
-//		T4.addActionListener(this);
-//		
+		JFrame Fen = new JFrame();
+		Fen.setTitle("Project Ebike");
+		//Fen.setSize(1700, 1000);
+		Fen.setSize(1000, 600);
+	    Fen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    Fen.setLocationRelativeTo(null);	
+	    Fen.setLayout(new GridLayout(16, 4));
+		T1 = new JTextField(30);		
+		T2 = new JTextField(30);
+		T3 = new JTextField(10);
+		T4 = new JTextField(10);
+		T5 = new JTextField(10);
+		T6 = new JTextField(10);
+		T7 = new JTextField(10);
+		T8 = new JTextField(10);
+		T1.setBackground(Color.cyan);
+		T2.setBackground(Color.cyan);
+		T3.setBackground(Color.cyan);
+		T4.setBackground(Color.cyan);
+		T5.setBackground(Color.cyan);
+		T6.setBackground(Color.cyan);
+		T7.setBackground(Color.cyan);
+		T8.setBackground(Color.cyan);
+		
+		L1 = new JLabel("Quel est votre point de départ? ");		
+		L2 = new JLabel("Quel est votre point d'arrivée? ");
+		L3 = new JLabel("Quelle est votre taille en cm? ");
+		L4 = new JLabel("Quel est votre poids total en comptant le poids du vélo en kg? ");
+		L5 = new JLabel("Quel est le coeff de frottement en pourcentage? ");
+		L6 = new JLabel("Quelle est la pente de la route? ");
+		L7 = new JLabel("Quelle est la vitesse limite du velo? ");
+		L8 = new JLabel("Quelle est la vitesse du velo? ");
+	    Fen.getContentPane().add(L1);
+	    Fen.getContentPane().add(T1);
+	    Fen.getContentPane().add(L2);
+	    Fen.getContentPane().add(T2);
+	    Fen.getContentPane().add(L3);
+	    Fen.getContentPane().add(T3);
+	    Fen.getContentPane().add(L4);
+	    Fen.getContentPane().add(T4);
+	    Fen.getContentPane().add(L5);
+	    Fen.getContentPane().add(T5);
+	    Fen.getContentPane().add(L6);
+	    Fen.getContentPane().add(T6);
+	    Fen.getContentPane().add(L7);
+	    Fen.getContentPane().add(T7);
+	    Fen.getContentPane().add(L8);
+	    Fen.getContentPane().add(T8);
+
+		T1.addActionListener(this);
+		T2.addActionListener(this);
+		T3.addActionListener(this);
+		T4.addActionListener(this);
+		T5.addActionListener(this);
+		T6.addActionListener(this);
+		T7.addActionListener(this);
+		T8.addActionListener(this);
+		
+		Fen.setVisible(true);
+			
 	}
-	
-	
+
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		    String text = T1.getText();
-		    T1.selectAll();
+		   
+		    String S1 = T1.getText();
+		    String S2 = T2.getText();
+		    String S3 = T3.getText();
+		    String S4 = T4.getText();
+		    String S5 = T5.getText();
+		    String S6 = T6.getText();
+		    String S7 = T7.getText();
+		    String S8 = T8.getText();
+		    System.out.println("S1 = "+S1+" S2 = "+S2+" S3 = "+S3+" S4 = "+S4 +"S5 = " + S5 + "S6 = " + S6);
 		}
 	}
 
