2a0124701e400fe80df0b437557249711041d48c
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-06 09:27:09 +0200

    Update Segment.java

--- ProgrammeInterpolation/Segment.java
+++ ProgrammeInterpolation/Segment.java
@@ -1,9 +1,13 @@
 class Segment{
 	
+	private static final double E_1; // energie requise pour un mètre de pente unité avec des conditions de base
+	private static final double E_0; // energie requise pour un mètre de pente nulle avec des conditions de base
+	
 	// constructeur du segment à partir des point de départ et d'arivée
-	public Segment(Point a, Point b){
+	public Segment(Point a, Point b, Vent v){
 		debut=a;
 		fin=b;
+		vent=v;
 	}
 	
 	/** retourne les paramètres
@@ -13,11 +17,12 @@ class Segment{
 	 * 	-p.getx() + p.gety() est l'énergie instantanée nécessaire sur une route d'exactement 45°
 	 */
 	public Point parametres(){
-		return new Point(50,0.5); // valeur générique pour l'instant
+		return new Point(E_1-E_0,E_0); // valeur générique pour l'instant, on changera en fonction du vent
 	}
 	
 	private Point debut;
 	private Point fin;
+	private Vent vent;
 	
 	// Plus tard, les paramètres de la route dépendront des conditions, que l'on déterminera en fonction des coordonées de départ et d'arivée
 }
