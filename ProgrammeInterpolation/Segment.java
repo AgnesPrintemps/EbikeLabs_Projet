29f7b0c07f7f063c6aad14901f34ce2cb96a22ce
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-06 09:38:38 +0200

    Update Segment.java

--- ProgrammeInterpolation/Segment.java
+++ ProgrammeInterpolation/Segment.java
@@ -1,7 +1,7 @@
 class Segment{
 	
-	private static final double E_1; // energie requise pour un mètre de pente unité avec des conditions de base
-	private static final double E_0; // energie requise pour un mètre de pente nulle avec des conditions de base
+	private static final double E_1=50; // energie requise pour un mètre de pente unité avec des conditions de base
+	private static final double E_0=0.5; // energie requise pour un mètre de pente nulle avec des conditions de base
 	
 	// constructeur du segment à partir des point de départ et d'arivée
 	public Segment(Point a, Point b, Vent v, Cycliste c, Velo b){
