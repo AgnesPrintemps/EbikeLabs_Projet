c9a0ed37c9fb5a6441b7f023f4d3a265b38aed2d
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-06 11:16:22 +0200

    Add files via upload

--- ProgrammeInterpolation/Segment.java
+++ ProgrammeInterpolation/Segment.java
@@ -1,15 +1,14 @@
 class Segment{
 	
-	private static final double E_1=50.0; // energie requise pour un mètre de pente unité avec des conditions de base (valeur arbitraire)
-	private static final double E_0=0.5; // energie requise pour un mètre de pente nulle avec des conditions de base (valeur arbitraire)
 	
 	// constructeur du segment à partir des point de départ et d'arivée
-	public Segment(Point a, Point b, Vent v, Cycliste c, Velo b){
+	public Segment(Point a, Point b, Vent v, Cycliste c, Velo b, double t){
 		debut=a;
 		fin=b;
 		vent=v;
 		cycliste=c;
 		velo=b;
+		temperature=t;
 	}
 	
 	/** retourne les paramètres
@@ -19,14 +18,44 @@ class Segment{
 	 * 	-p.getx() + p.gety() est l'énergie instantanée nécessaire sur une route d'exactement 45°
 	 */
 	public Point parametres(){
-		return new Point(E_1-E_0,E_0); // valeur générique pour l'instant, on changera en fonction du vent
+		
+		double f = getFrottement();
+		double m = cycliste.getPoids()+velo.getPoids();
+		double Vs = velo.getVLim();
+		double Cxa = m/250/(Vs*Vs);
+		double Cxb = 324*f*Cxa;
+		
+		double resPes = m*g;
+		
+		double massevolu = 1.292*273.15/(273.15+temperature);
+		
+		double airefron = cycliste.aireFron();
+		
+		double resAira = 0.5*massevolu*airefron*Cxa;
+		double resAirb = 0.5*massevolu*airefron*Cxb;
+		
+		double resFrot = m*g*f;
+
+		double Fra = resPes+resAira;
+		double Frb = resAirb+resFrot;
+		double vitesse = 12;
+		
+		return new Point(Fra*vitesse,Frb*vitesse); // valeur générique pour l'instant, on changera en fonction du vent
 	}
+	
+	public double getFrottement(){
+		 return 0.01; // valeur arbitraire
+	 }
+	 
+	 
 	
 	private Point debut;
 	private Point fin;
 	private Vent vent;
 	private Cycliste cycliste;
 	private Velo velo;
+	private double temperature;
+	private static final double g = 9.81;
 	
 	// Plus tard, les paramètres de la route dépendront des conditions, que l'on déterminera en fonction des coordonées de départ et d'arivée
 }
