7d5603494f2c9d1b048aa13ee146b827b674c9e1
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-07 09:11:39 +0200

    Update Chemin.java

--- ProgrammeInterpolation/Chemin.java
+++ ProgrammeInterpolation/Chemin.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 class Chemin{
 	
 	// constructeur
-	public Chemin(String nom){
+	public Chemin(String nom, Vent v, Cycliste c, double t){
 
 		DonneesGPS d=new DonneesGPS(nom); // on recupere les données GPS
 		LinkedList<Point> l = new LinkedList<Point>(); // les distances à l'origine
@@ -17,13 +17,12 @@ class Chemin{
 		
 		Vent v=new Vent(1,0); // on prend en compte le vent (a modifier plus tard)
 		Cycliste c=new Cycliste(); // on prend en compte le cycliste (a modifier plus tard)
-		Velo b=new Velo(); // on prend en compte le velo (a modifier plus tard)
 		double t=15; // on prend en compte la température (a modifier plus tard)
 		
 		for(int i=1; i<d.getTailleEchantillon()-1;i++){ // puis, pour chaque point
 			ltot+=Point.distance(new Point(d.getLongitude(i), d.getLatitude(i)), new Point(d.getLongitude(i-1), d.getLatitude(i-1))); // on ajoute la distance entre le point courant et le dernier point pour trouver la distance à l'origine du point
 			l.add(new Point(ltot,d.getAltitude(i))); // on rajoute à la liste des points un nouveau point indiquant sa distance à l'origine et son altitude
-			Segment s=new Segment(new Point(d.getLongitude(i-1), d.getLatitude(i-1)),new Point(d.getLongitude(i), d.getLatitude(i)), v, c , b, t); // puis, on crée un segment entre ces points
+			Segment s=new Segment(new Point(d.getLongitude(i-1), d.getLatitude(i-1)),new Point(d.getLongitude(i), d.getLatitude(i)), v, c, t); // puis, on crée un segment entre ces points
 			t.add(s.parametres()); // et on ajoute ses paramètres
 		}
 		
