560c43b681d7095a146283e916adafd4d9111d81
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-07 09:45:48 +0200

    Update Point3.java

--- ProgrammeInterpolation/Point3.java
+++ ProgrammeInterpolation/Point3.java
@@ -1,24 +1,17 @@
 class Point3 extends Point{
 		
 	// constructeur
-	public Point3(double x, double y, double z){
+	public Point3(double x, double y, double yprime){
 		super(x,y);
-		this.z=z;
+		this.yprime=yprime;
 	}
 	
 	/** retourne la pente du point
 	 * @return la pente du point
 	 */
-	public double getz(){
-		return z;
+	public double getyprime(){
+		return yprime;
 	}
 	
-	/** retourne les coordonées cartésiennes du point courant en coordonées polaires
-	 * @return les coordonées cartésiennes du point courant en coordonées polaires
-	 */
-	public Point3 polToCart(){
-		return new Point3(getx()*getz(),gety()*getz(),getz()-DonneesGPS.getRayonTerre());
-	}
-	
-	private double z;
+	private double yprime;
 }
