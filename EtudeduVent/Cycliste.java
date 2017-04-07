ca28f4028acf8a39da02042b821f8c8c360870db
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-07 10:02:59 +0200

    Update Cycliste.java

--- EtudeduVent/Cycliste.java
+++ EtudeduVent/Cycliste.java
@@ -1,8 +1,29 @@
 public class Cycliste {
+	
+	public Cycliste(String n, double p, Velo v, double t, double a){
+    		nom=n;
+    		poids=p;
+		velo=v;
+    		talle=t;
+		aireFrontale=a;
+  	}
+	
+	public double getPoids(){
+		return poids+velo.getPoids();
+	}
+	
+	public double getVLim(){
+		return velo.getVLim();
+	}
+	
+	public double getAireFron(){
+		return aireFrontale;
+	}
 
 	private String nom;
-	private int poids; // en kg
+	private double poids; // en kg
 	private Velo velo;
 	private double taille; // en cm
+	private double aireFrontale;
 
 }
