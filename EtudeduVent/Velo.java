f9ec3a13250c6072b6b6d791b6b84491c17fb740
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-10 15:54:50 +0200

    Update Velo.java

--- EtudeduVent/Velo.java
+++ EtudeduVent/Velo.java
@@ -1,12 +1,26 @@
 public class Velo {
+	
+	public Velo(file f){
+   	 	try{
+			Scanner sc=new Scanner(f);
+			String temp = sc.nextLine();
+			poids=Double.parseDouble(temp);
+			temp = sc.nextLine();
+			vitessemax=Double.parseDouble(temp);
+			temp = sc.nextLine();
+			batterie=Double.parseDouble(temp);
+		}
+		catch(FileNotFoundException e){}
+		sc.close()
+  	}
   
-  public Velo(double p, double v, double t){
-    poids=p;
-    vitessemax=v;
-    batterie=b;
-  }
+	public Velo(double p, double v, double t){
+    		poids=p;
+    		vitessemax=v;
+   		batterie=b;
+ 	}
   
-  public double getPoids(){
+  	public double getPoids(){
 		return poids;
 	}
 	
@@ -14,7 +28,7 @@ public class Velo {
 		return vitessemax;
 	}
 
-  private final double poids; //en kg
-  private final double vitessemax; // en km/h
-  private final double batterie; // en Watts
+  	private final double poids; //en kg
+  	private final double vitessemax; // en km/h
+  	private final double batterie; // en Watts
 }
