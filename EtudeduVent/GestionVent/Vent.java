9f53a6cbcc00eff3d661ecee5004a423c4af667b
JacquesLindsay <jacques.lindsay@orange.fr>
2017-04-10 16:21:27 +0200

    Update Vent.java

--- EtudeduVent/GestionVent/Vent.java
+++ EtudeduVent/GestionVent/Vent.java
@@ -1,17 +1,13 @@
 package GestionVent;
 import java.util.Scanner;
+import java.util.Scanner;
 
 public class Vent {
 
 	private String direction;
-	private double force;
+	private double vitesse;
 	
-	public Vent (String d, double f){
-		direction = d;
-		force = f;
-	}
-	
-	public Vent (int d, double f){
+	public Vent (int d, double v){
 		switch(d){
 			case 1 : direction = "Nord"; break;
 			case 2 : direction = "Nord-Nord-Ouest"; break;
@@ -24,22 +20,22 @@ public class Vent {
 			case 9 : direction = "Sud"; break;
 			case 10 : direction = "Sud-Sud-Est"; break;
 			case 11 : direction = "Sud-Est"; break;
-			case 12 : direction "Est-Sud-Est"; break;
+			case 12 : direction = "Est-Sud-Est"; break;
 			case 13 : direction = "Est"; break;
-			case 13 : direction = "Est-Nord-Est"; break;
-			case 14 : direction = "Nord-Est"; break;
-			case 15 : direction = "Nord-Nord-Est"; break;
+			case 14 : direction = "Est-Nord-Est"; break;
+			case 15 : direction = "Nord-Est"; break;
+			case 16 : direction = "Nord-Nord-Est"; break;
 			default : direction = "Erreur"; break;
 		}
-		force = f;
+		vitesse = v;
 	}
 	
 	public double getVitesse(){
-		return f;
+		return vitesse;
 	}
 	
 	public double getAngle(){
-		double pi = Math.pi;
+		double pi = Math.PI;
 		switch(direction){
 			case "Nord" : return pi/2;
 			case "Nord-Nord-Ouest" : return 5*pi/8;
@@ -50,15 +46,15 @@ public class Vent {
 			case "Sud-Ouest" : return -3*pi/4;
 			case "Sud-Sud-Ouest" : return -5*pi/8;
 			case "Sud": return -pi/2;
-			case "Sud-Sud-Est": -3*pi/8
-			case "Sud-Est": return -pi/4
-			case "Est-Sud-Est": return -pi/8
+			case "Sud-Sud-Est": return -3*pi/8;
+			case "Sud-Est": return -pi/4;
+			case "Est-Sud-Est": return -pi/8;
 			case "Est": return 0;
-			case "Est-Nord-Est": return pi/8
-			case "Nord-Est": return pi/4
-			case "Nord-Nord-Est": return 3*pi/8 
+			case "Est-Nord-Est": return pi/8;
+			case "Nord-Est": return pi/4;
+			case "Nord-Nord-Est": return 3*pi/8;
 			default: return 0;
 		}
 				
-	
+	}
 }
